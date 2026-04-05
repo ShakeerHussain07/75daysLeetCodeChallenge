bbc8d1d2ff90bec176d57615d06d3e24f4595659
SHAKEER HUSSAIN SHAIK <shakeershaik1729@gmail.com>
2026-04-05 08:04:34 +0530

    Time: 0 ms (100%), Space: 44 MB (51.55%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int mlen = matrix.length;
+        int n = matrix[0].length;
+        
+        int left = 0;
+        int right = mlen * n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Convert 1D index to 2D index
+            int row = mid / n;
+            int col = mid % n;
+            
+            int midValue = matrix[row][col];
+            
+            if (midValue == target) {
+                return true;
+            } 
+            else if (midValue < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+}
